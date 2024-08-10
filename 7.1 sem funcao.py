3b4fefd86cb3d1195ac1a15bd8aabcb19f0e40b2
Thomaz <140722585+ThomazFerraz@users.noreply.github.com>
2024-08-10 16:29:03 -0300

    inicio exercicio funcoes

---
+++ 7.1 sem funcao.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------------------------------------
+
+# Código sem funções:
+
+cadastro_completo = False
+
+while not cadastro_completo:
+    nome = input('Digite seu nome: ')
+    idade = int(input('Insira sua idade: '))
+    rua = input('Insira a rua onde mora: ')
+    bairro = input('Insira o bairro em que você mora: ')
+    cidade = input('Insira a cidaded em que você mora: ')
+
+    print('Aqui estão os dados que você nos enviou:\n')
+    print(f'Nome: {nome};')
+    print(f'Idade: {idade} anos;')
+    print(f'Endereço completo: {rua}, {bairro}, {cidade}')
+
+    confirmacao = bool(input('Confirme os dados acima. Digite "True" se estiver correto ou "False" se algo estiver errado.'))
+
+    if confirmacao:
+        print(f'Obrigado pela confirmação, {nome}. Em breve você receberá uma carta com as informações para seguir seu procedimento...')
+        cadastro_completo = True
+
+    else:
+        print(f'Que pena, {nome}, vamos corrigir seus dados: \n')
